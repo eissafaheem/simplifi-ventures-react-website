57f3231aee93666a96902d19c6080ab0f30f1a74
Eissa Faheem <eissafaheem@gmail.com>
2023-10-22 11:25:28 +0530

    Feat: navbar complete

--- src/app/compenents/navbar/Navbar.tsx
+++ src/app/compenents/navbar/Navbar.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import NavbarStyles from './Navbar.module.css'
 
 function Navbar() {
   return (
-    <div>
-      
+    <div className={NavbarStyles['navbar-container']}>
+      <div className={NavbarStyles["brand-name"]}>
+        Simplifi Ventures
+      </div>
+      <div className={NavbarStyles["routes"]}>
+        <a href="">Team</a>
+        <a href="">Studio</a>
+        <a href="">Resources</a>
+      </div>
     </div>
   )
 }
